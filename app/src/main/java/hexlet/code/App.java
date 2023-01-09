f81907c9062c1a4b8f23b10f52abfc147cfbafb5
anasasiia <sheldoncooperbuga@gmail.com>
2023-01-09 16:31:30 +0400

    added App.class

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,39 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+                    if (!isProduction()) {
+                        config.plugins.enableDevLogging();
+                    }
+//                    config.enableWebjars();
+                });
+        addRoutes(app);
+        app.before(ctx -> ctx.attribute("ctx", ctx));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
